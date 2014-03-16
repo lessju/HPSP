b332ebf8154fb779c3ab8e3ee324d6e3fd13fac4
Ian Fenech Conti <ianfc89@gmail.com>
2014-03-16 22:35:32 +0100

    Responsive Changes
    Responsive Changes

--- dev/js/home.js
+++ dev/js/home.js
@@ -11,6 +11,14 @@
 		$( ".menu" ).click(function() {
 		  $( ".nav ul" ).toggleClass('nav-open');
 		});
+
+		$('html').click(function() {
+		  $( ".nav ul" ).toggleClass('nav-open');
+		});
+
+		$('.nav').click(function(event){
+		    event.stopPropagation();
+		});
 	});
 
 	$(function() {
