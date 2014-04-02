e5030cf186988c2521be19405f4b1e6e9e5e83bb
Ian Fenech Conti <ianf89@gmail.com>
2014-04-02 11:14:59 +0200

    Layout Changes

--- dev/js/shared.js
+++ dev/js/shared.js
@@ -2,8 +2,20 @@ $(document).ready(function() {
 	menuHandler();
 	$('.loader').on('webkitAnimationEnd animationend' , function(event){   
 		$('.loader').hide();
-	});
+	});	
+	resizePage();
 });
+
+$( window ).resize(function() {
+ 	resizePage();
+
+});
+
+function resizePage(){
+	// Check if navbox exists
+	if ($('.nav-box').length)
+		 $('.main-content').height($('.nav-box ul li:last-child').position().top + 200);
+}
 
 function menuHandler(){
 	$( ".menu" ).click(function() {
